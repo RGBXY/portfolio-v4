6eb5c5b0f6afb97bd4bcfa08235587ae4613b215
RGBXY <derrenamadeo@gmail.com>
2025-12-24 03:15:39 +0700

    add:hero-section|photo-transition

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
-import HeroSection from "@/components/section/home-page/hero-section";
+import AboutSection from "@/components/home/about-section";
+import HeroSection from "@/components/home/hero-section";
 
 export default function Home() {
   return (
     <div className="h-full w-full flex justify-center">
-      <div className="w-6xl h-full">
+      <div className="w-6xl h-full space-y-40">
         <HeroSection />
+        <AboutSection />
       </div>
     </div>
   );
---
+++ components/home/hero-section.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <div className="w-full h-full pt-20 flex items-center justify-center text-center">
+      <div className="space-y-8 flex flex-col items-center w-full">
+        <div className="border p-1.5 rounded-full bg-linear-to-br/srgb from-white via-gray-100 to-white shadow">
+          <div className="border flex gap-2 text-gray-800 font-medium items-center w-full h-full rounded-full py-1 px-4">
+            <span>👋 Let's Get Connect</span>
+          </div>
+        </div>
+
+        <div className="text-4xl font-medium leading-tight tracking-tighter text-text-primary md:text-7xl md:leading-24">
+          <div className="flex items-center gap-6">
+            <h1 className="text-black">Hey, I'm</h1>
+            <div className="w-24 h-24 border p-2 rounded-2xl bg-linear-to-br/srgb from-white via-gray-100 to-white shadow hover:rotate-8 transition">
+              <Image className="w-full h-full shadow-inner border border-gray-300 rounded-xl overflow-hidden flex items-center justify-center" src={"/image/profile.webp"} alt="profile" width={300} height={300} />
+            </div>
+            <h1 className="text-black">Derren Amadeo!</h1>
+          </div>
+          <h1 className="text-black">Front-End Developer</h1>
+        </div>
+
+        <p className="max-w-sm mx-auto w-full">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla atque nobis eveniet et.</p>
+
+        <div className="mt-10 w-full flex items-center justify-center">
+          <div className="h-70 w-87.5 border border-black rounded-xl -rotate-4 bg-gray-300 z-5 hover:-rotate-8 hover:scale-110 transition"></div>
+          <div className="h-70 w-87.5 border border-black rounded-xl rotate-4 translate-y-6 -ml-10 bg-gray-300 z-4 hover:rotate-8 hover:scale-110 transition"></div>
+          <div className="h-70 w-87.5 border border-black rounded-xl -ml-10 -rotate-4 bg-gray-300 z-3 hover:-rotate-8 hover:scale-110 transition"></div>
+          <div className="h-70 w-87.5 border border-black rounded-xl -ml-10 rotate-4 translate-y-6 bg-gray-300 hover:rotate-8 hover:scale-110 transition"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
